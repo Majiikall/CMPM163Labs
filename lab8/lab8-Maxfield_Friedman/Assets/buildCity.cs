2b7433bdea366005b3e188b23f7ad5ff91deb7c1
Maxfield Friedman <maxfield@artifacttech.com>
2020-05-26 16:18:30 -0700

    Working on lab 8

---
+++ lab8/lab8-Maxfield_Friedman/Assets/buildCity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class buildCity : MonoBehaviour
+{
+	public GameObject[] buildings;
+	public int mapWidth = 20;
+	public int mapHeight = 20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int x = 0; x < mapHeight; x++)
+        {
+        	for(int y = 0; y < mapWidth; y++)
+        	{
+        		Vector3 pos = new Vector3(y,0,x);
+        		int n = Random.Range(0, buildings.Length);
+        		Instantiate(buildings[n], pos, Quaternion.identity);
+        	}
+        }
+    }
+
+}
